86f176d737045d079120b9031f15cad21d5d98cf
anmol <anmolpokhrel46@gmail.com>
2023-02-01 22:04:03 +0530

    updated helpers

--- src/components/TransactionList/fetchTrades.ts
+++ src/components/TransactionList/fetchTrades.ts
@@ -1,29 +1,32 @@
-import { formatEther } from "ethers/lib/utils.js"
-import { getStatus, getSymbol } from "@/utils"
-import { BigNumber } from "ethers"
-import { Trade } from "./type"
-import { getTradesIDsByUser, getTrade } from "@/helpers/getterHelpers"
+import { formatEther } from "ethers/lib/utils.js";
+import { getStatus, getSymbol } from "@/utils";
+import { BigNumber } from "ethers";
+import { Trade } from "./type";
+import { getTradesIDsByUser, getTrade } from "@/helpers/getterHelpers";
 
-export const getTradeList = async (amount: number, address: `0x${string}` | undefined) => {
-  const tradeIds = await getTradesIDsByUser(address as string)
+export const getTradeList = async (
+    amount: number,
+    address: `0x${string}` | undefined
+) => {
+    const tradeIds = await getTradesIDsByUser(address as string);
 
-  const trades: Trade[] = []
-  await Promise.all(
-    tradeIds.slice(0, amount).map(async (tradeId: BigNumber) => {
-      const trade = await getTrade(Number(tradeId._hex))
-      await trades.push({
-        id: Number(trade.id),
-        seller: trade.seller,
-        buyer: trade.buyer,
-        tokenToSell: await getSymbol(trade.tokenToSell),
-        tokenToBuy: await getSymbol(trade.tokenToBuy),
-        amountOfTokenToSell: formatEther(trade.amountOfTokenToSell),
-        amountOfTokenToBuy: formatEther(trade.amountOfTokenToBuy),
-        deadline: Number(trade.deadline),
-        status: getStatus(trade.status),
-      })
-    })
-  )
+    const trades: Trade[] = [];
+    await Promise.all(
+        tradeIds.slice(0, amount).map(async (tradeId: BigNumber) => {
+            const trade = await getTrade(Number(tradeId._hex));
+            await trades.push({
+                id: Number(trade.id),
+                seller: trade.seller,
+                buyer: trade.buyer,
+                tokenToSell: await getSymbol(trade.tokenToSell),
+                tokenToBuy: await getSymbol(trade.tokenToBuy),
+                amountOfTokenToSell: formatEther(trade.amountOfTokenToSell),
+                amountOfTokenToBuy: formatEther(trade.amountOfTokenToBuy),
+                deadline: trade.deadline.toString(),
+                status: getStatus(trade.status),
+            });
+        })
+    );
 
-  return trades
-}
+    return trades;
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,39 +1,43 @@
-import { BigNumber, ethers } from "ethers"
-import { Alchemy, Network } from "alchemy-sdk"
-import { signer } from "../constants/interact"
-import { ERC20_ABI } from "../constants/ERC20_ABI"
+import { BigNumber, ethers, Signer } from "ethers";
+import { Alchemy, Network } from "alchemy-sdk";
+import { ERC20_ABI } from "../constants/ERC20_ABI";
+import { getSigner } from "@/helpers/getterHelpers";
 
-const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_GOERLI_NETWORK
+const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_GOERLI_NETWORK;
 
 const settings = {
-  apiKey: ALCHEMY_API_KEY,
-  network: Network.ETH_GOERLI,
-}
+    apiKey: ALCHEMY_API_KEY,
+    network: Network.ETH_GOERLI,
+};
 
-const alchemy = new Alchemy(settings)
+const alchemy = new Alchemy(settings);
 
 export function formatInt(value: BigNumber) {
-  return Number(value._hex) / 10 ** 18
+    return Number(value._hex) / 10 ** 18;
 }
 export const getStatus = (value: number) => {
-  switch (Number(value)) {
-    case 0:
-      return "Pending"
-    case 1:
-      return "Completed"
-    case 2:
-      return "Cancelled"
-    case 3:
-      return "Expired"
-    case 4:
-      return "Withdrawn"
-    default:
-      return "Unknown"
-  }
-}
+    switch (Number(value)) {
+        case 0:
+            return "Pending";
+        case 1:
+            return "Completed";
+        case 2:
+            return "Cancelled";
+        case 3:
+            return "Expired";
+        case 4:
+            return "Withdrawn";
+        default:
+            return "Unknown";
+    }
+};
 
 export const getSymbol = async (tokenAddress: string) => {
-  const tokenContract = await new ethers.Contract(tokenAddress, ERC20_ABI, signer)
-  const symbol = await tokenContract.symbol()
-  return symbol
-}
+    const tokenContract = new ethers.Contract(
+        tokenAddress,
+        ERC20_ABI,
+        (await getSigner()) as Signer
+    );
+    const symbol = await tokenContract.symbol();
+    return symbol;
+};
